993d63fce41aee5e5b97fc4dbe3057b5096322d8
32gede <federico.riato0@gmail.com>
2024-10-10 19:36:04 +0200

    Cambiamenti LOGIN con GOOGLE

--- app/src/main/java/com/example/progetto/data/model/LoginUtils.java
+++ app/src/main/java/com/example/progetto/data/model/LoginUtils.java
@@ -17,6 +17,7 @@ public class LoginUtils {
         editor.putString(KEY_DISPLAY_NAME, displayName);
         editor.apply();
     }
+
     public static void saveGoogleLoginState(Context context, boolean isLoggedIn) {
         SharedPreferences sharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
@@ -26,13 +27,14 @@ public class LoginUtils {
 
     public static boolean isLoggedIn(Context context) {
         SharedPreferences sharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
-        return sharedPreferences.getBoolean(KEY_IS_LOGGED_IN, false)||sharedPreferences.getBoolean(KEY_IS_LOGGED_IN_WITH_GOOGLE, false);
+        return sharedPreferences.getBoolean(KEY_IS_LOGGED_IN, false) || sharedPreferences.getBoolean(KEY_IS_LOGGED_IN_WITH_GOOGLE, false);
     }
 
     public static String getDisplayName(Context context) {
         SharedPreferences sharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
         return sharedPreferences.getString(KEY_DISPLAY_NAME, null);
     }
+
     public static void clearLoginState(Context context) {
         SharedPreferences sharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
