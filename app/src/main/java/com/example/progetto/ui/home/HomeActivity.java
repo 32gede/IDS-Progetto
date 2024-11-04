cbde5f906e0d7db19f5d42e0190aeb1d952cbca5
32gede <federico.riato0@gmail.com>
2024-11-04 20:07:28 +0100

    Aggiunta prodotto FUNZIONANTE(in parte)

--- app/src/main/java/com/example/progetto/ui/home/HomeActivity.java
+++ app/src/main/java/com/example/progetto/ui/home/HomeActivity.java
@@ -1,6 +1,15 @@
 package com.example.progetto.ui.home;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.progetto.R;
 
 public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+    }
 }
